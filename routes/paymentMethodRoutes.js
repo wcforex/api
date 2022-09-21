2f8e953fb0a3a2f95897d7a3d28d2fcda5eccdbe
princepatrickbondzie <ngekow@gmail.com>
2022-09-21 13:31:08 +0000

    payment method fix

--- routes/paymentMethodRoutes.js
+++ routes/paymentMethodRoutes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { isAdmin } = require('../middleware/authorize')
 const { createPaymentMethod, getPaymentMethod, getPaymentMethods, updatePaymentMethod, deletePaymentMethod } = require('../controllers/paymentMethodController');
 
-router.get('/all', isAdmin, getPaymentMethods);
-router.get('/:id', isAdmin, getPaymentMethod);
+router.get('/all', getPaymentMethods);
+router.get('/:id', getPaymentMethod);
 router.post('/create', isAdmin, createPaymentMethod);
 router.patch('/:id', isAdmin, updatePaymentMethod);
 router.patch('/:id', isAdmin, deletePaymentMethod);
