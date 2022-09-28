f6180aed1c6e60f78c1daca3619a737f353d7acd
princepatrickbondzie <ngekow@gmail.com>
2022-09-28 21:31:35 +0000

    fix

--- routes/packageRoutes.js
+++ routes/packageRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { isAdmin, verifyToken } = require('../middleware/authorize')
+const { isAdmin } = require('../middleware/authorize')
 const { createPackage, deletePackage, getPackage, getPackages, updatePackage } = require('../controllers/packageController');
 
 router.get('/all', getPackages);
-router.get('/:id', getPackage);
+router.get('/:id', isAdmin, getPackage);
 router.post('/create', isAdmin, createPackage);
 router.patch('/:id', isAdmin, updatePackage);
 router.delete('/:id', isAdmin, deletePackage);
